452ed6c4e13cad537fc56f2ae1af8736e129d2f2
maxl95 <maxl.fischer@gmx.de>
2019-08-21 11:09:40 +0200

    update configuration.h and mainpage.c

--- TFT/src/User/Menu/MainPage.h
+++ TFT/src/User/Menu/MainPage.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _MAINPAGE_H_
 #define _MAINPAGE_H_
 
@@ -19,25 +18,3 @@ void menuMain(void);
 
 #endif
 
-=======
-#ifndef _MAINPAGE_H_
-#define _MAINPAGE_H_
-
-enum
-{
-  MAIN_HEAT,
-  MAIN_MOVE,
-  MAIN_HOME,
-  MAIN_PRINT,
-  MAIN_EXTRUDE,
-  MAIN_FAN,
-  MAIN_SETTING,
-  MAIN_LEVELING,
-  TOTAL_MAIN,
-};
-
-void menuMain(void);
-
-#endif
-
->>>>>>> 6117d986a314238a4f0a5ecdc478c1888b7b561d
